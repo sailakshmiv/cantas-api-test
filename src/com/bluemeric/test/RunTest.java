a60af4c70ab4c19835770ef0b04a2c966e4a2c9a
goPaddle <testgopaddle@gmail.com>
2016-11-04 20:42:08 +0530

    Update RunTest.java

--- src/com/bluemeric/test/RunTest.java
+++ src/com/bluemeric/test/RunTest.java
@@ -20,11 +20,11 @@ import com.bluemeric.common.TestApp;
 import com.bluemeric.common.Util;
 
 public class RunTest  implements ITestListener {
-	static String projectHome = System.getProperty("PROJECT_HOME") + "/";
-	static String endpoint = System.getProperty("APP_ENDPOINT"); 
+//	static String projectHome = System.getProperty("PROJECT_HOME") + "/";
+//	static String endpoint = System.getProperty("APP_ENDPOINT"); 
 
-//	static String projectHome = System.getProperty("user.dir")+"/";//System.getProperty("PROJECT_HOME") + "/";
-//	static String endpoint = "localhost:30000";
+	static String projectHome = System.getProperty("user.dir")+"/";//System.getProperty("PROJECT_HOME") + "/";
+	static String endpoint = "localhost:30000";
 	
 	List<XmlSuite> xmlSuites;
 	
